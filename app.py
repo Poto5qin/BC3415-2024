6b41658630e1f8cfab5388aede28fcdbabbf1296
Poto5qin <160687728+Poto5qin@users.noreply.github.com>
2025-01-15 08:25:51 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
 from flask import Flask
+from flask import render_template, request
+
+app = Flask(_name_)
+
+@app.route("/")
+def index():
+    return(render_template("index.html"))
+
+if _name_ == "_main_":
+
+    
